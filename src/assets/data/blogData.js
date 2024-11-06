550c25eec61eaaa4393791772b70b01caf3a0b25
Your Name <you@example.com>
2024-11-06 00:14:22 -0500

    files updated

---
+++ src/assets/data/blogData.js
@@ -0,0 +1,47 @@
+// import images from all-images/blog-img directory
+import img01 from "../all-images/blog-img/blog-1.jpg";
+import img02 from "../all-images/blog-img/blog-2.jpg";
+import img03 from "../all-images/blog-img/blog-3.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    title: "The best way to drive cars",
+    author: "Chirag",
+    date: "06 November, 2024",
+    time: "12am",
+    imgUrl: img01,
+    description:
+    "Adjust your mirrors, seat, and controls before you put it in gear. Of course, this is where to start. The new angle? Set a memory position if you have one, as many cars now do. And spend some time getting the perfect angle for the rearview and sideview mirrors, because soon they'll all be replaced by cameras, and you'll miss them <hr /> Keep your insurance card up to date, in your car or on your phone. Many car-insurance carriers now offer electronic identification. Download their app, keep it updated. Check to see if it overrides your passcode in an emergency -- or if you're totes paranoid, set your lock screen to a picture of your policy card. If none of this sounds familiar, make sure you keep the latest, active version of the printed card on your person and not in the car. Keep your license plate mounted and clear of debris. This is for the rest of us, so we can report you when we need to. But it also will keep you from getting pulled over by the cop who can't see it, and therefore thinks you have something to hide. <hr /> Sit up straight and set the proper driving position. This isn't your living room couch. Hands on the wheel, chest at least a foot away from the airbag module, with the gauges framed by the wheel and a clear line of sight to traffic lights from below the windshield frame.",
+    quote:
+      "You car is a job, a life, a home, a weapon, a buddy.",
+  },
+
+  {
+    id: 2,
+    title: "If your car battery is down",
+    author: "Chirag",
+    date: "06 November, 2024",
+    time: "12am",
+    imgUrl: img02,
+    description:
+    " Position the booster vehicle close to the dead one. Have someone with a working vehicle park as close to the dead car as possible. In most cases, the battery is located beneath the hood, so parking hood-to-hood is ideal. Sometimes, though, the battery may be located elsewhere (like the trunk), so check your owner’s manual if you have trouble finding it. If you suspect your car battery is dying and don’t see any obvious signs of corrosion, try jump-starting it first to see if you can recharge it before replacing the battery altogether. It could’ve died because of a fluke, like leaving your lights on or being exposed to cold weather. If you have to jump-start it frequently, however, it’s probably time to see a mechanic or replace the battery. If your battery is located somewhere other than beneath the hood, Maynes says it’s called a “relocated battery.” Vehicles with these batteries have “jump points underneath the hood,” so refer to your owner’s manual for instructions on how to jump-start your car.",
+    quote:
+      "How to Tell If Your Car Battery Is Dead or Dying",
+  },
+
+  {
+    id: 3,
+    title: "The best way to give trip",
+    author: "Chirag",
+    date: "06 November, 2024",
+    time: "12am",
+    imgUrl: img03,
+    description:
+      " Gift an Instagram-worthy excursion. Giving someone surf lessons on a trip they already have planned to Costa Rica or a wine tasting for their upcoming Bordeaux vacation builds in an opportunity for what Mogilner Holmes calls “peak experiences”—memorable moments of trips that tend to stick with us. Since we tend to photograph peak experiences, they prove a big part of making a trip a lasting one, she says. Remember to keep your recipient’s interests in mind. It seems like a no-brainer, but research finds the opposite. Says Mogilner Holmes: “Gift givers tend to give things that they like without taking into account what the recipient likes.” Just because 12-course, three-and-a-half hour tasting menus are your thing, it doesn't mean they're everyone's. If your friend is more the outdoors-type, consider a guided hike. If they wouldn't be caught dead in a tent, find ways to infuse a little luxury into the trip. Supplementing a trip with add-ons travelers on a budget often skip—namely private car services and nicer hotel rooms—can be an ideal present for any traveler, says Steele. After all, the ease of stepping off of a plane in a foreign country only to see a well-dressed stranger holding a sign with your name on it is pretty much priceless.",
+    quote:
+      "If you’ve ever wanted to gift someone an experience or trip, odds are you’ve dealt with the following first world problem… how exactly to give the gift of travel in a fun way that preserves the wanderlusty magi",
+  },
+];
+
+export default blogData;
